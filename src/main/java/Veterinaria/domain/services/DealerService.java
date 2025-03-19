98f9a614623160a702fc415b57708948e5f7c77d
luisgomez10238 <luisgomez10238@gmail.com>
2025-03-18 19:15:59 -0500

    Update DealerService.java
    dealerservice

--- src/main/java/Veterinaria/domain/services/DealerService.java
+++ src/main/java/Veterinaria/domain/services/DealerService.java
@@ -1,5 +1,15 @@
 package Veterinaria.domain.services;
 
+import Veterinaria.domain.models.Order;
+import Veterinaria.ports.OrderPort;
+
 public class DealerService {
+    OrderPort orderPort;
+
+    public void consultOrder(Order order){
+        if (orderPort.existOrder(order.getOrderID()) ){
+            
+        }
+    }
 
 }
