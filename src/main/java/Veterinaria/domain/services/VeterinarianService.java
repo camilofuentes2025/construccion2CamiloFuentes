6991451359c0cc855a85e45e308b6eaf1032b591
luisgomezOrrego <luis.gomez69@correo.tdea.edu.co>
2025-03-30 02:20:03 -0500

    a
    a

--- src/main/java/Veterinaria/domain/services/VeterinarianService.java
+++ src/main/java/Veterinaria/domain/services/VeterinarianService.java
@@ -1,5 +1,29 @@
 package Veterinaria.domain.services;
 
+import Veterinaria.domain.models.Pet;
+import Veterinaria.ports.PetPort;
+
 public class VeterinarianService {
+    PetPort petPort;
+
+    public void register_attention(Pet pet)throws Exception{
+        if (!petPort.existPet(pet.getPetID())) {
+            throw new Exception("La mascota no esta registrada en el sistema");
+        }
+
+        
+
+
+
+
+
+    }
+
+
+    public void consult_medical_history(){
+        
+    }
+
+
 
 }
